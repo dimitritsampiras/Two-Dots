6d78ce13239cf92a0af2f8b7f596f954795a99ca
Dimitri Tsampiras <dimitri.tsampiras@gmail.com>
2020-04-25 13:47:33 -0400

    Adds first draft of DotT enumerable type

---
+++ src/DotT.ts
@@ -0,0 +1,19 @@
+/**
+ * @file DotT.java
+ * @description Defines an enumerable type for Dot colors
+ * @author Dimitri Tsampiras
+ * @date April 1, 2020
+ */
+
+
+/**
+ * DotT enumerable
+ */
+enum DotT {
+  Red = "R", 
+  Blue = "B", 
+  Green = "G", 
+  Orange = "O"
+}
+
+console.log(DotT.Red);
