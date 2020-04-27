4129d688dacd458f83659e749a389cbbe52f21a8
Dimitri Tsampiras <dimitri.tsampiras@gmail.com>
2020-04-27 17:12:21 -0400

    Formats code of PointT

--- src/PointT.ts
+++ src/PointT.ts
@@ -5,23 +5,23 @@
  * @date April 24, 2020
  */
 
-import { Interval } from "./Interval"
+import { Interval } from "./Interval";
 
- /**
-  * PointT class
-  */
+/**
+ * PointT class
+ */
 export class PointT {
   readonly x: number;
   readonly y: number;
 
   /**
-	 * Constructor for PointT Class
-	 * @param xc x coordinate
-	 * @param yc y coordinate
-	 */
-	public constructor(xc: number, yc: number) {
-		this.x = xc;
-		this.y = yc;
+   * Constructor for PointT Class
+   * @param xc x coordinate
+   * @param yc y coordinate
+   */
+  public constructor(xc: number, yc: number) {
+    this.x = xc;
+    this.y = yc;
   }
 
   /**
@@ -29,29 +29,25 @@ export class PointT {
    * @param d object representing domain
    */
   public isWithinDomain(d: Interval): boolean {
-		return this.x >= d.min && this.x <= d.max;
-	}
-  
+    return this.x >= d.min && this.x <= d.max;
+  }
+
   /**
    * Determines if reference point is withint range
    * @param r object representing range
    */
-	public isWithinRange(r:  Interval): boolean {
-		return this.y >= r.min && this.y <= r.max;
+  public isWithinRange(r: Interval): boolean {
+    return this.y >= r.min && this.y <= r.max;
   }
-  
-  /**
-	 * Method to check if a pair of coordinates are adjacent to reference coordinate
-	 * @param p external PointT object
-	 * @return boolean based of adjacency of external coordinate
-	 */
-	public isAdjacent(p: PointT): boolean {
-		if (p.y == this.y && Math.abs(p.x - this.x) == 1)
-			return true;
-		if (p.x == this.x && Math.abs(p.y - this.y) == 1)
-			return true;
-		return false;
-	}
-  
-}
 
+  /**
+   * Method to check if a pair of coordinates are adjacent to reference coordinate
+   * @param p external PointT object
+   * @return boolean based of adjacency of external coordinate
+   */
+  public isAdjacent(p: PointT): boolean {
+    if (p.y == this.y && Math.abs(p.x - this.x) == 1) return true;
+    if (p.x == this.x && Math.abs(p.y - this.y) == 1) return true;
+    return false;
+  }
+}
